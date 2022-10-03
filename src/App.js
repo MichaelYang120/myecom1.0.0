362e4a9cffa506ffee281cc9b4400661bf71032f
MichaelYang120 <97741880+MichaelYang120@users.noreply.github.com>
2022-10-03 11:57:06 -0500

    Added content area
    I added a content area and added it the the app.js file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import Header from "./myEcom/Header";
 import Footer from "./myEcom/Footer";
+import Content from "./myEcom/Content";
 import { render } from "@testing-library/react";
 
 function App() {
     return (
   <div>
     <Header />
-    content
+    <Content />
     <Footer />
   </div>
   )
---
+++ src/myEcom/Content.js
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+
+export default class Content extends Component {
+  render() {
+    return (
+        <section className="main-content">
+            hello world this is my content
+        </section>
+    );
+  }
+}
+
