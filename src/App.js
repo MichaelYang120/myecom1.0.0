dcb35e860cf438e764d752ac4beb7461bef7ab91
MichaelYang120 <97741880+MichaelYang120@users.noreply.github.com>
2022-10-03 21:41:51 -0500

    added FeatureContent and custom.scss
    In this commit, I added a FeatureContent area, showing a carousel of featured images.  I also added some more web packages from bootstrap.  Added custom.scss file to hold global variables to change carousel speed.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import Header from "./myEcom/Header";
 import Footer from "./myEcom/Footer";
 import Content from "./myEcom/Content";
 import { render } from "@testing-library/react";
+import FeatureContent from "./myEcom/FeatureContent";
+// @import "custom";
+
 
 function App() {
     return (
   <div>
     <Header />
-    <Content />
+    {/* <Content /> */}
+    <FeatureContent />
     <Footer />
   </div>
   )
--- src/myEcom/Content.js
+++ src/myEcom/Content.js
@@ -1,5 +1,6 @@
 import React, {Component} from "react";
 
+
 export default class Content extends Component {
   render() {
     return (
