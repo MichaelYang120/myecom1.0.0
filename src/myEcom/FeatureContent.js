e672f5f692c6678431e40a2aa62ce3e737e8c0f1
MichaelYang120 <97741880+MichaelYang120@users.noreply.github.com>
2022-10-04 09:11:46 -0500

    removed white space, and extra code

--- src/myEcom/FeatureContent.js
+++ src/myEcom/FeatureContent.js
@@ -7,7 +7,7 @@ function FeatureContent() {
         }}>
       <Carousel.Item interval={2500}>
         <img
-          className="d-block w-100 active"
+          className="d-block w-100"
           src="https://images.unsplash.com/photo-1496747611176-843222e1e57c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2073&q=80"
           alt="First slide"
         />
